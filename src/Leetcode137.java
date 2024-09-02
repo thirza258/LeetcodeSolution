a1811d72e47c52f95f53c237875320df2c1af0da
thirzq <thirzahmad@gmail.com>
2024-09-02 21:08:16 +0700

    change java ver

--- src/Leetcode137.java
+++ src/Leetcode137.java
@@ -1,8 +1,17 @@
 public class Leetcode137 {
     public int singleNumber(int[] nums) {
-        for(int i = 1; i < nums.length; i++) {
-            
+        int[] count = new int[32];
+        int result = 0;
+
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if ((nums[j] >> i & 1) == 1) {
+                    count[i]++;
+                }
+            }
+            result |= (count[i] % 3) << i;
         }
-       
+
+        return result;
     }
 }
