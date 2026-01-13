8315a5509391879690b5546c69101eb3d1274567
thirzq <thirzahmad@gmail.com>
2026-01-13 23:52:46 +0700

    add more solution

---
+++ src/Leetcode345.java
@@ -0,0 +1,36 @@
+public class Leetcode345 {
+    public String reverseVowels(String s) {
+        if(s == null || s.isEmpty()) {
+            return s;
+        }
+
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            while (left < right && !isVowel(chars[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(chars[right])) {
+                right--;
+            }
+
+            if (left < right) {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    private boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+}
